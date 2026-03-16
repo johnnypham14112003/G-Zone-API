0a84f0fd969ad33292efc7d8f716949e270f0ab3
Johnny Pham <johnnypham14112003@gmail.com>
2026-03-17 03:08:46 +0700

    refactor: business model UserAddress, Account

--- GZone.Service/BusinessModels/Request/Account/AccountRequest.cs
+++ GZone.Service/BusinessModels/Request/Account/AccountRequest.cs
@@ -5,20 +5,12 @@ namespace GZone.Service.BusinessModels.Request.Account
     public class AccountRequest
     {
         public Guid? Id { get; set; }
-
-        // --- AUTHENTICATION & COMMON INFO ---
-        public string? RefreshToken { get; set; }
-        public DateTime? RefreshTokenExpiryTime { get; set; }
-        public string? AvatarUrl { get; set; }
-        public string? Username { get; set; }
-        public string? PasswordHash { get; set; }
         public string? Email { get; set; }
         [MaxLength(12)] public string? Phone { get; set; } // Số điện thoại chính chủ
         public string? FullName { get; set; }
         [MaxLength(30)] public string? Role { get; set; }
         [MaxLength(30)] public string? Status { get; set; }
         public bool IsActive { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         // --- CUSTOMER SPECIFIC FIELDS (Nullable) ---
         public DateTime? DateOfBirth { get; set; }
